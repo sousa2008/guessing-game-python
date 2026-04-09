33d82515f3806dc4afa8b4ac29366cf71b6ed325
Lucas de Sousa <lucas.2008.sousa@gmail.com>
2026-04-09 11:56:48 -0300

    V1.2: Criação do sistema de recordes e ranking

--- Guessing game number.py
+++ Guessing game number.py
@@ -1,5 +1,7 @@
 from random import randint
 import time
+
+#variaveis gerais
 pergunta = "S"
 n = 0
 cont = 0
@@ -8,6 +10,17 @@ pontuacao = 0
 perda = 0
 recorde = 0
 menu = 1
+#variaveis dos recordes: modo dificil
+recordeDificil1 = 999
+recordeDificil2 = 999
+nome1 = ""
+nome2 = ""
+
+#varaiveis dos recordes: modo limitado
+recordeLimit1 = 0
+recordeLimit2 = 0
+nomeLimit1 = ""
+nomeLimit2 = ""
 
 # Estilização do inicio do game
 print ('Carregando...')
@@ -17,10 +30,10 @@ print('=-=-= Jogo da advinhação =-=-=')
 
 # menu do jogo
 while pergunta == "S":
-    menu = int(input('Você deseja:\n1-Jogar\n2-Sair\n'))
+    menu = int(input('Você deseja:\n1-Jogar\n2-Sair\n3-Recordes\n'))
     # validação para o menu não quebrar
-    if menu not in [1, 2]:
-        print("Valor inválido.\nDigite 1 ou 2.")
+    if menu not in [1, 2, 3]:
+        print("Valor inválido.\nDigite 1, 2 ou 3.")
         menu = int(input('Você deseja:\n1-Jogar\n2-Sair\n'))
 # definiçao do botão 2 como saida
     if menu == 2:
@@ -71,10 +84,16 @@ while pergunta == "S":
                 # definição da perda por pontuação
                 if dificuldade == 1:
                     perda = 9
+                    if pontuacao <= 0:
+                        pontuacao = 1 #não deixar zerar
                 elif dificuldade == 2:
                     perda = 9.5
+                    if pontuacao <= 0:
+                        pontuacao = 1 #não deixar zerar
                 elif dificuldade == 3:
                     perda = 11
+                    if pontuacao <= 0:
+                        pontuacao = 1 #não deixar zerar
                 pontuacao -= perda
             print("Você acertou!\nO número era: \033[32;1m{}\033[m\nE você precisou de \033[32;1m{}\033[m tentativas". format(aleatorio, cont))
             print("Sua pontuação foi de: \033[32;1m{}\033[m.".format(pontuacao))
@@ -83,6 +102,19 @@ while pergunta == "S":
             if pontuacao > recorde:
                 recorde = pontuacao
             print("Seu recorde de pontuação atual é de: \033[32;1m{}\033[m.".format(recorde))
+            #registrando recorde do modo dificil.
+            if dificuldade == 3:
+                if pontuacao > recordeDificil1:
+                    nome2 = nome1
+                    recordeDificil2 = recordeDificil1
+
+                    nome1 = str(input("Digite o nome que irá para os recordes: "))
+                    recordeDificil1 = pontuacao
+
+                elif pontuacao > recordeDificil2:
+                    nome2 = str(input("Digite o nome que irá para os recordes: "))
+                    recordeDificil2 = pontuacao
+
             pontuacao = 0    #zerar pontuacao para próxima rodada
 
 # Modo de jogo 4
@@ -99,16 +131,28 @@ while pergunta == "S":
                         print('Muito baixo.')
                         cont += 1
                         n = int(input('Tente novamente: '))
-                    if n > aleatorio:
+                    elif n > aleatorio:
                         print('Muito alto.')
                         cont += 1
                         n = int(input('Tente novamente: '))
-                    if n == aleatorio:
+                    elif n == aleatorio:
                         print("Você conseguiu antes das chances acabarem!\nO número era: \033[32;1m{}\033[m\nVocê usou \033[32;1m{}\033[m tentativas".format(aleatorio, cont))
                         cont += 7
-                    if cont == 7 and n != aleatorio:
+                    elif cont == 7 and n != aleatorio:
                         print("Você perdeu.\nO valor era: \033[32;1m{}\033[m".format(aleatorio))
                         print("Tente novamente.")
+
+            #Definição dos recordes do modo limitado
+            if n == aleatorio:
+                if cont < recordeLimit1:
+                    nomeLimit2 = nomeLimit1
+                    recordeLimit2 = recordeLimit1
+
+                    nomeLimit1 = str(input("Digite o nome que irá para os recordes: "))
+                    recordeLimit1 = cont
+                elif cont < recordeLimit2:
+                    nomeLimit2 = str(input("Digite o nome que irá para os recordes: "))
+                    recordeLimit2 = cont
 
 # Modo de jogo 5
         if dificuldade == 5:
@@ -140,7 +184,30 @@ while pergunta == "S":
             print("Você finalizou o modo INFINIT.\nVocê acertou \033[32;1m{}\033[m números e teve \033[32;1m{}\033[m tentativas".format(pontuacao, cont))
 
 # reinicio do laço iniciado no começo do programa
-        pergunta = str(input("Deseja jogar de novo? [S/N]")).upper().strip()
+        pergunta = str(input("Deseja voltar ao menu? [S/N] ")).upper().strip()
+
+#menu de recordes
+    if menu == 3:
+       while True:
+            print("")
+            print("Você entrou no modo recordes.")
+            print("Os recordes estão disponiveis para o modo dificil.")
+            menu = int(input("Digite o modo que você deseja ver os recordes:\n1-Difícil\n2-Limitado\n3-Voltar\n"))
+            if menu == 1:
+                print("")
+                print("Você selecionou o modo \033[31;1mDIFÍCIL\033[m")
+                print("Abaixo estão os 2 melhores colocados no modo: ")
+                print("1- \033[32;1m{}\033[m - \033[31;1m{}\033[m".format(recordeDificil1, nome1))
+                print("2- \033[32;1m{}\033[m - \033[31;1m{}\033[m".format(recordeDificil2, nome2))
+            elif menu == 2:
+                print("")
+                print("Você selecionou o modo \033[31;1mLIMITADO\033[m")
+                print("Abaixo estão os 2 melhores colocados no modo: ")
+                print("1- \033[32;1m{}\033[m tentativas - \033[31;1m{}\033[m".format(recordeLimit1, nomeLimit1))
+                print("2- \033[32;1m{}\033[m tentativas - \033[31;1m{}\033[m".format(recordeLimit2, nomeLimit2))
+            elif menu == 3:
+                break
+
     # validação contra valores inválidos
     if pergunta not in ['S', 'N']:
         print('Digite um valor válido [S/N].')
